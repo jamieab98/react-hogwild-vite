dca5ead95786ba03d908044f9f0e706dba23fc88
Jamie Bryson <jamieab98@gmail.com>
2025-10-15 18:18:34 -0400

    made it so clicking would show the special components

--- src/components/hogtile.jsx
+++ src/components/hogtile.jsx
@@ -1,12 +1,23 @@
 import React from "react";
+import { useState } from "react";
+import styles from "./hogtile.module.css";
 
 function Hogtiles({hogData}) {
+    const [selectedHog, setSelectedHog] = useState(null);
+
+    function handleClick(hog) {
+        setSelectedHog(hog);
+    }
 
     return(
         <div className = "ui grid container">
             {hogData.map((hog) => (
-                <div className = "ui eight wide column">
+                <div aria-label="hog card" key={hog.name} onClick={() => handleClick(hog)}>
                     <h3>Name: {hog.name} </h3>
+                    <div className = {selectedHog===hog ? `${styles.extraInfo}` : `${styles.hiddenInfo}`}>Specialty: {hog.specialty}</div>
+                    <div className = {selectedHog===hog ? `${styles.extraInfo}` : `${styles.hiddenInfo}`}>{hog.greased ? "Greased" : "Not greased"}</div>
+                    <div className = {selectedHog===hog ? `${styles.extraInfo}` : `${styles.hiddenInfo}`}>Weight: {hog.weight}</div>
+                    <div className = {selectedHog===hog ? `${styles.extraInfo}` : `${styles.hiddenInfo}`}>The Highest Medal Achieved:{hog["highest medal achieved"]}</div>
                     <img src = {hog.image}></img>
                 </div>
             ))}
