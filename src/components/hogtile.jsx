30d2b48c4af7bce1e9eb54f69ae0ca205342ff8f
Jamie Bryson <jamieab98@gmail.com>
2025-10-15 17:08:42 -0400

    working on getting the information into the hogtile component

--- src/components/hogtile.jsx
+++ src/components/hogtile.jsx
@@ -3,7 +3,14 @@ import React from "react";
 function Hogtiles({hogData}) {
 
     return(
-         console.log(hogData)
+        <div>
+            {hogData.map((hog) => (
+                <div>
+                    <h3>Name: {hog.name} </h3>
+                    <img src = {hog.image}></img>
+                </div>
+            ))}
+        </div>
     )
 }
 
